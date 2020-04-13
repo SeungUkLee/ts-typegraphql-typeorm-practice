c2a59f7e538b74b3810be1a3b876fa60f653fa98
SeungUkLee <lsy931106@gmail.com>
2020-04-13 16:42:36 +0900

    resolvers

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import "reflect-metadata"
 import { createConnection } from "typeorm";
 import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
+import { BookResolver } from "./resolvers/BookResolver";
 
 // This function makes it easier to initialize every single library that we use in this project.
 async function main() {
     const conn = await createConnection(); // create a new connection to our db.
-    const schema = await buildSchema(); // generate our GraphQL schema
+    const schema = await buildSchema({
+        resolvers: [BookResolver],
+    }); // generate our GraphQL schema
     const server = new ApolloServer({ schema }); // initialize our apollo server
     await server.listen(4000);
     console.log("Server has started!");
 }
+
+main().catch((err) => console.log(err));
